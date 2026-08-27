ddc7b762addf48b3a28b35b681b1f6542f66d257
Colton Hicks <github@coltonhicks.com>
2026-08-26 19:18:30 -0600

    xHigh Audit (#6)

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,10 +1,11 @@
 import type { APIRoute } from "astro";
+import { organization } from "../data/organization";
 
 const routes = ["/", "/work", "/technology", "/about", "/team", "/organization", "/donate"];
 
 export const GET: APIRoute = () => {
   const urls = routes
-    .map((route) => `  <url><loc>https://www.atomsforhumanity.org${route}</loc></url>`)
+    .map((route) => `  <url><loc>${organization.domain}${route}</loc></url>`)
     .join("\n");
 
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>
